6259c7577d817a058379434f8f1f6d2a934e3387
roman <r.kylik.py@gmail.com>
2023-03-29 13:44:03 +0300

    Добавлено паттерн адаптер

--- Structural/Adapter.py
+++ Structural/Adapter.py
@@ -5,10 +5,10 @@ def Roulette_in_inch(cls):
             setattr(self.wrapped, "length", getattr(self.wrapped, "length") * 39.37)
             setattr(self.wrapped, "units", "inch")
         def __getattr__(self, name) :
-            attr = getattr(self.wrapped, name)
-
-            return attr
+            return getattr(self.wrapped, name)
     return Wrapper
+
+
 @Roulette_in_inch
 class Roulette():
     def __init__(self, x):
@@ -17,5 +17,5 @@ class Roulette():
     def length_measurement(self):
         return f'довжина становить {self.length}  {self.units}'
 
-d= Roulette(5)
+d= Roulette(18)
 print(d.length_measurement())

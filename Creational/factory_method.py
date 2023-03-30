20f1013b243a7f1553f9c7fa4863662bc7cd7e51
roman <r.kylik.py@gmail.com>
2023-03-30 16:03:26 +0300

    Виправлено Фабричний Паттерн після вказівок ментора

--- Creational/factory_method.py
+++ Creational/factory_method.py
@@ -4,43 +4,77 @@ from abc import *
 class Product:
     def __init__(self):
         self.type = ""
-    @abstractmethod
-    def get_type(self) -> str:
+
+    def __str__(self) -> str:
         return self.type
 
 """Конкретний продукт"""
 class  PlaneProduct(Product):
     def __init__(self):
         self.type = "Plane"
-    def get_type(self) -> str:
+    def __str__(self) -> str:
         return self.type
 
 class  TankProduct(Product):
     def __init__(self):
         self.type_pr = "Tank"
-    def get_type(self) -> str:
+    def __str__(self) -> str:
         return self.type_pr
-class  ArtilleryProduct(Product):
-    def __init__(self):
-        self.type = "Artillery"
-    def get_type(self) -> str:
-        return self.type
 
-"""Клас творець."""
-class FactoryProduct():
-    def create_product(self, product_type: Product) -> Product:
-        """
-        Factory Method
-        """
-        factory_dict = {
-            "Plane": PlaneProduct,
-            "Tank": TankProduct,
-            "Artillery": ArtilleryProduct
-        }
-        return factory_dict[product_type]()
 
+"""Інтерфейс фабрики (творець) із фабричним методом."""
+class Creator():
+    @abstractmethod
+    def factory_method(self, product_type: Product) -> Product:
+
+        pass
+    @abstractmethod
+    def some_logik (self):
+        """Певна логіка творця, та виклик фабричного методу"""
+        return self.factory_method()
+    def __str__(self):
+        return f'Фабрика створила {self.some_logik()}'
+
+
+class FactoryTank(Creator):
+    def factory_method(self) -> Product:
+        return TankProduct()
+
+    def some_logik (self):
+        """Певна логіка творця, та виклик фабричного методу"""
+        return self.factory_method()
+    def __str__(self):
+        return f'Фабрика створила {self.some_logik()}'
+
+class FactoryPlane(Creator):
+    def factory_method(self) -> Product:
+        return PlaneProduct()
+
+    def some_logik (self):
+        """Певна логіка творця, та виклик фабричного методу"""
+        return self.factory_method()
+
+    def __str__(self):
+        return f'Фабрика створила {self.some_logik()}'
+
+
+def client_code(creator: str) -> None:
+    creators = {
+        "T": FactoryTank,
+        "P": FactoryPlane
+    }
+    return creators[creator]()
 
 if __name__ == '__main__':
-    factory = FactoryProduct()
-    my_product = factory.create_product("Tank")
-    print(f'Виготовлено продукт {my_product.get_type()}')
+    new = client_code('P')
+    print (new)
+
+"""Було створено інтерфейс продукту та його екземпляри - конкретні продукти (TankProduct, PlaneProduct). 
+Також створено інтевфейс для Creator(творця продутків), та від повідні творці для конкретних продуктів, із фабричними методами які повертають нам конкретні продукти.
+Також створена метод клієнського коду , якому ми кажемо який нам продукт потрібен,  а він нам повертає -> Творця продукту -> Сам продукт.
+ 
+ 
+Оскільки мінусом цього паттерна є велика кількість класів творців( для кожного продутку потрібен свій творець).
+Тому на попередньому уроці я показував один клас творця без його інтерфейсу, який повертав нам потрібний продукт(необхідний продукт задавався в клієнському коді)."""
+
+

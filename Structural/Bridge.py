7b319ee3a19da9ee2ab294014cff480d8e0fb7a8
roman <r.kylik.py@gmail.com>
2023-04-04 20:23:52 +0300

    Виправлено паттерн Міст

--- Structural/Bridge.py
+++ Structural/Bridge.py
@@ -1,20 +1,67 @@
-from random import randrange
+from abc import *
 
-class Color:
-    def __init__(self):
-        self. red = 'red'
-        self.black = 'black'
-        self.pink = "pink"
+class Engine(ABC):
+    """Інтерфейс двигуна ( Перша ієрархія класів)"""
+    def __init__(self, type):
+        self.type = type
 
-class Auto:
-    def __init__(self, name):
+    @abstractmethod
+    def start_engine(self):
+        pass
+    @abstractmethod
+    def spin_the_wheels(self):   #крутити колеса
+        pass
+
+
+class ConcretEngine(Engine):
+    """Реалізація інтерфейсу Engine ( Друга ієрархія класів)"""
+    def start_engine(self):
+        return "is started"
+    def spin_the_wheels(self):
+        return "is going"
+
+
+
+class Auto(ABC):
+    """Інтерфейс автомобіля ( Друга ієрархія класів)"""
+
+
+    def __init__(self, name: str, engine: Engine):
         self.name = name
-        self.color = [Color().red, Color().black, Color().pink][randrange(3)]
+        self.engine = engine
+        self._status = "not started"
+    @abstractmethod
+    def drive(self):
+        pass
 
+    @abstractmethod
+    def start_car(self):
+        pass
+
+
+class Skoda(Auto):
+    """Реалізація інтерфейсу Auto ( Друга ієрархія класів)"""
+    def start_car(self):
+        if self._status == "not started":
+            a = self.engine.start_engine()
+            self._status = a
+            return f"Автомобіль {self.name} {self._status}."
+        else:
+            return f"Автомобіль {self.name} уже заведений."
+
+    def drive(self):
+        if self._status == "is started":
+            return f"Автомобіль {self.name} {self.engine.spin_the_wheels()}"
+        else:
+            print("Спочатку заведемо авто, тоді поїдемо")
+            self.start_car()
+            return self.drive()
 
 
 if __name__ == '__main__':
-    new_auto = Auto("Skoda")
-    new_auto2 = Auto("Renault")
-    print(f"Створено новий автомобіль  {new_auto.name} кольору {new_auto.color}")
-    print(f"Створено новий автомобіль  {new_auto2.name} кольору {new_auto2.color}")
+
+    new_engine = ConcretEngine("Benzine")
+    new_auto = Skoda("Skoda", new_engine)
+
+    print(new_auto.start_car())
+    print(new_auto.drive())
